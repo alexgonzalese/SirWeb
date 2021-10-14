6e02cf5a6a4e5d68dce248091a0fce770c0e5781
Alexander <Alexgonzalese@gmail.com>
2021-10-14 07:48:16 -0500

    Se crea LoginForm y la funcion para llamar la API

--- src/api/user.js
+++ src/api/user.js
@@ -1,26 +1,39 @@
-import {basePath, apiVersion} from './config';
+import { basePath, apiVersion } from "./config";
 
-export function signUpApi(data){
-    const url =`${basePath}/${apiVersion}/sign-up`;
-    const params={
-        method:"POST",
-        body: JSON.stringify(data),
-        headers:{
-            "Content-Type": "application/json"
-        }
-    };
+export function signUpApi(data) {
+  const url = `${basePath}/${apiVersion}/sign-up`;
+  const params = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    return fetch(url, params)
-        .then(response =>{
-            return response.json();
-        })
-        .then(result =>{
-            if(result.user){
-                return {ok: true, message: "Usuario Creado"};
-            }
-            return {ok: false, message: result.message};
-        })
-        .catch(err=>{
-            return {ok: false, message: err.message};
-        });
+  return fetch(url, params)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      if (result.user) {
+        return { ok: true, message: "Usuario Creado" };
+      }
+      return { ok: false, message: result.message };
+    })
+    .catch((err) => {
+      return { ok: false, message: err.message };
+    });
+}
+
+export function signInApi(data) {
+  const url = `${basePath}/${apiVersion}/sign-in`;
+  const params = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  
 }
